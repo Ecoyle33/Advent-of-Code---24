c54b1808aaf5939912779031ef77a109b21353cc
Euan Coyle <92370561+Ecoyle33@users.noreply.github.com>
2024-12-09 20:18:05 +0000

    convert numbers in list to int, then sort

--- Q1.py
+++ Q1.py
@@ -1,4 +1,4 @@
-import numpy as np
+# import numpy as np
 
 #Create two lists, each list contains one column from input.txt
 list1 = []
@@ -10,5 +10,11 @@ with open('input.txt', 'r') as f:
         list1.append(entry1)
         list2.append(entry2)
 
-print(f'{list1}')
-print(f'{list2}')
+# print(f'{list1}')
+# print(f'{list2}')
+
+list1 = [int(x) for x in list1]
+list2 = [int(x) for x in list2]
+
+list1.sort()
+list2.sort()

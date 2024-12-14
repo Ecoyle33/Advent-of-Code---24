469097b3d3c89b3bb833d4cd82a7b7e3d8e23a51
Euan Coyle <92370561+Ecoyle33@users.noreply.github.com>
2024-12-14 17:37:54 +0000

    Formatted lists correctly

--- Q2/Q2.py
+++ Q2/Q2.py
@@ -4,7 +4,10 @@ with open('Q2\input.txt', 'r') as f:
     
     for line in f:
         
-        entry = line.strip().split('\n')
+        entry = line.strip().split()
+
+        entry = [int(x) for x in entry]
+
         inputList.append(entry)
 
-print(inputList)
+

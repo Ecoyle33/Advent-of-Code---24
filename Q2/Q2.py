71379b06f961ef256fb22af8683aba12c04fdb27
Euan Coyle <92370561+Ecoyle33@users.noreply.github.com>
2024-12-14 17:52:36 +0000

    value difference check implemented

--- Q2/Q2.py
+++ Q2/Q2.py
@@ -5,9 +5,23 @@ with open('Q2\input.txt', 'r') as f:
     for line in f:
         
         entry = line.strip().split()
-
         entry = [int(x) for x in entry]
-
         inputList.append(entry)
 
+numSafeLists = 0
 
+for array in inputList:
+    
+    for i in range(len(array) - 1):
+        
+        value1 = array[i]
+        value2 = array[i + 1]
+
+        diff = abs(value2 - value1)
+
+        if ((diff > 0) and (diff <= 3)):
+            numSafeLists += 1
+        
+        break
+
+print(numSafeLists)      

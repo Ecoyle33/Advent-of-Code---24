b77eed5e63e44702b5a00e7cc16a5c0b9bcee66b
Euan Coyle <92370561+Ecoyle33@users.noreply.github.com>
2024-12-14 22:09:11 +0000

    Part 1 complete

--- Q2/Q2.py
+++ Q2/Q2.py
@@ -1,4 +1,4 @@
-inputList = []
+list1 = []
 
 with open('Q2\input.txt', 'r') as f:
     
@@ -6,5 +6,46 @@ with open('Q2\input.txt', 'r') as f:
         
         entry = line.strip().split()
         entry = [int(x) for x in entry]
-        inputList.append(entry)
+        list1.append(entry)
 
+num_safe_lists = 0
+
+for array in list1:
+
+    diff_list = []
+    # Iterate over each element in list
+    for i in range(len(array) - 1):
+        diff = array[i+1] -  array[i]
+        diff_list.append(diff)
+
+    # If all elements are increasing:
+    if (all(x > 0 for x in diff_list)):
+
+        j = 0
+        for i in range(len(diff_list)):
+            
+            if (diff_list[i] > 0) and (diff_list[i] <= 3):
+                j += 1
+        
+        if j == len(diff_list):
+            num_safe_lists += 1
+
+    # If all elements are decreasing
+    elif (all(x < 0 for x in diff_list)):
+        
+        j = 0
+        abs_diff = [abs(x) for x in diff_list]
+
+        for i in range(len(abs_diff)):
+                
+                if (abs_diff[i] > 0) and (abs_diff[i] <= 3):
+                    j += 1
+        
+        if j == len(abs_diff):
+                num_safe_lists += 1
+
+    # If the elements stay the same:
+    else:
+        continue
+
+print(num_safe_lists)
